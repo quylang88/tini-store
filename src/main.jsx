0bff4d3d44ee1f062bd5717846801b12c6cf2744
quyln <quylang88@gmail.com>
2026-01-29 18:41:25 +0900

    SpeedInsights Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 // Register Service Worker for PWA capabilities (Notifications)
 if ("serviceWorker" in navigator) {
@@ -23,5 +24,6 @@ if ("serviceWorker" in navigator) {
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
   </StrictMode>,
 );
